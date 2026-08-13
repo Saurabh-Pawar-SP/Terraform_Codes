1a120d73f4d75656e8d589fb8cda2830ebe05d74
Saurabh Pawar <saurabhpawar1802@gmail.com>
2026-08-13 22:59:03 +0530

    S3 Bucket file added

---
+++ S3_Bucket.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket        = "my-terraform-demo-bucket-123456"
+  force_destroy = true
+
+  tags = {
+    Name        = "my-s3-bucket"
+    Environment = "dev"
+    Owner       = "Saurabh"
+    Project     = "Terraform"
+    Department  = "IT"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "my_bucket_encryption" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
